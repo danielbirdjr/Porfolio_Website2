12bb3da5fa5b013d29d999aa09a316ee484859a3
Daniel Bird <danielbirdpersonal@gmail.com>
2024-09-16 12:56:19 -0400

    Project section: ProgressAI photo and details

--- Portfolio_Website/src/App.jsx
+++ Portfolio_Website/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import ProgressAIImage from './assets/ProgressAI2.png';
+import ProgressAIImage from './assets/ProgressAI3.png';
 import ComingSoonImage from './assets/coming_soon.jpg';
 
 
@@ -131,7 +131,7 @@ function App() {
           </div>
           <div id='projects' className='project_section section'>
             <h2 className='section_header'>Projects</h2>
-            <a href="https://training-app-project.vercel.app/" target='_blank'>
+            <a href="https://progressai.vercel.app/" target='_blank'>
               <div className='project_container'>
                 <div className='project_image_container'>
                   <img src={ProgressAIImage} alt="ProgressAI Daniel Bird" />
@@ -143,17 +143,18 @@ function App() {
                       <g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className='arrow_icon'><path d="M1 13 13 1M4 1h9v9"></path></g>
                     </svg>
                   </h3>
-                  <p>Developed a fitness tracking app that leverages AI to provide personalized workout programs and continuous optimization based on user data such as workout intensity, training volume, sleep quality, nutrition, and stress.</p>
+                  <p>Developed a fitness tracking app that will leverage AI to provide personalized workout programs and continuous optimization based on user data such as workout intensity, training volume, sleep quality, nutrition, and stress.</p>
                   <p>Created a sleek and minimalistic interface ensuring a seamless and intuitive user experience.</p>
-                  <p>Managed the full project lifecycle from concept to deployment, utilizing Vite for development efficiency and Firebase for authentication.</p>
+                  <p>Managed the full project lifecycle from concept to deployment, utilizing Node.js for development efficiency and Firebase for authentication.</p>
                   <ul className='languages_used'>
                     <li><div className='languages_used_container'>React</div></li>
+                    <li><div className='languages_used_container'>Node.js</div></li>
                     <li><div className='languages_used_container'>JavaScript</div></li>
                     <li><div className='languages_used_container'>HTML</div></li>
                     <li><div className='languages_used_container'>CSS</div></li>
                     <li><div className='languages_used_container'>Firebase</div></li>
-                    <li><div className='languages_used_container'>SQL</div></li>
-                    <li><div className='languages_used_container'>Vite</div></li>
+                    <li><div className='languages_used_container'>AI</div></li>
+                    <li><div className='languages_used_container'>ML</div></li>
                   </ul>
                 </div>
               </div>
